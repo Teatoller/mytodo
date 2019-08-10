61728b678646037a73d22118c57f8a02eb464890
Teatoller <steven.otieno1@gmail.com>
2019-08-10 22:02:03 +0300

    (store): implement store
    - create store
    - add initial state
    - add middleware
    - import root reducer
    
    [Starts #013]

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
-import todoApp from "./reducers";
+import store from "./store";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-
-const store = createStore(todoApp);
 
 ReactDOM.render(
   <Provider store={store}>
--- src/store.js
+++ src/store.js
@@ -1,30 +1,37 @@
-import { createStore } from 'redux'
-import todoApp from './reducers'
-import {
-    addTodo,
-    toggleTodo,
-    setVisibilityFilter,
-    VisibilityFilters
-  } from './actions'
+import { createStore, applyMiddleware, compose } from "redux";
+import todoApp from "./reducers";
+import thunk from "redux-thunk";
 
-const store = createStore(todoApp, window.STATE_FROM_SERVER)
+// import {
+//   addTodo,
+//   toggleTodo,
+//   setVisibilityFilter,
+//   VisibilityFilters
+// } from './actions'
 
+const initialState = {};
 
-  
-  // Log the initial state
-  console.log(store.getState())
-  
-  // Every time the state changes, log it
-  // Note that subscribe() returns a function for unregistering the listener
-  const unsubscribe = store.subscribe(() => console.log(store.getState()))
-  
-  // Dispatch some actions
-  store.dispatch(addTodo('Learn about actions'))
-  store.dispatch(addTodo('Learn about reducers'))
-  store.dispatch(addTodo('Learn about store'))
-  store.dispatch(toggleTodo(0))
-  store.dispatch(toggleTodo(1))
-  store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED))
-  
-  // Stop listening to state updates
-  unsubscribe()
+const middleware = [thunk];
+
+const store = createStore(
+  todoApp,
+  initialState,
+  compose(
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+
+// console.log("store....", store.getState()) // Log the initial state
+// const unsubscribe = store.subscribe(() => console.log(store.getState()))
+
+// store.dispatch(addTodo('Learn about actions'))
+// store.dispatch(addTodo('Learn about reducers'))
+// store.dispatch(addTodo('Learn about store'))
+// store.dispatch(toggleTodo(0))
+// store.dispatch(toggleTodo(1))
+// store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED))
+
+// unsubscribe()
+
+export default store;
